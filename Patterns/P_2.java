633885bce2e58b2ff1e1ca4a0c9061bae764779a
kartikay-101 <k.matrix.hacked@gmail.com>
2024-04-07 02:22:32 +0530

    adding 2nd Pattern

---
+++ Patterns/P_2.java
@@ -0,0 +1,39 @@
+    /* 2nd Pattern: * * * * *
+                    *       *
+                    *       *
+                    *       *
+                    * * * * *   */
+
+import java.util.*;
+
+public class P_2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char ch;
+        do{
+        System.out.print("\nEnter the number of Rows: ");
+        int r = sc.nextInt();
+        System.out.print("Enter the number of Columns: ");
+        int c = sc.nextInt();
+        System.out.print("\n\tThe Pattern is: \n\n");
+            for(int i=0; i<r; i++){
+                for(int s=0; s<20; s++){
+                    System.out.print(" ");
+                }
+                for(int j=0; j<c; j++){
+                    if ( i==0 || j==0 || i==r-1 || j==c-1) {
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.print("\n\tDo you want to continue? (Y/N): ");
+            ch = sc.next().charAt(0);
+        }while(ch == 'y' || ch == 'Y');
+
+        sc.close();
+    }
+}
